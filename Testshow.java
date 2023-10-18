a655b15e96a33b457320aa45cd5b0a49eb95657a
Gilchrist123 <142879099+Gilchrist123@users.noreply.github.com>
2023-10-18 16:24:39 +0530

    Add files via upload

---
+++ Testshow.java
@@ -0,0 +1,16 @@
+package abc;
+
+public class Testshow {
+
+	public static void main(String[] args) {
+		Show n1 =new Show();
+		
+		n1.display(1);
+		
+		n1.display(10);
+		n1.display("abc");
+		n1.display(0.000);
+		
+	}
+
+}
