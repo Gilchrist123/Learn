65becb78ab93ba0e4ffac6f0c063285e49d0a71b
unknown <gilchristpereira10@gmail.com>
2023-10-18 16:50:31 +0530

    My first commit from loacl machine

--- Testshow.java
+++ Testshow.java
@@ -5,8 +5,8 @@ public class Testshow {
 	public static void main(String[] args) {
 		Show n1 =new Show();
 		
-		n1.display(1);
-		
+		n1.display(10);
+		sdsddsds
 		n1.display(10);
 		n1.display("abc");
 		n1.display(0.000);
